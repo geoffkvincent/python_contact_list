af8fca8251c0cb3afd5faf3cbca57f7ef7b736e9
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-28 16:48:51 -0600

    menu function

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ class Contacts:
         print('================')
         print('  Contact List  ')
         print('================')
-        print('please make a selection')
-        user_input = input()
+        self.menu()
 
     def menu(self):
+        print('please make a selection')
         print('1) View Contacts')
         print('2) Add Contact')
         print('3) Delete Contact')
         print('4) Print')
         print('5) Exit')
+        user_input = input()
+        self.menu_switch(user_input)
 
     def menu_switch(self)
 
