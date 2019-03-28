020ca434cb577f6e9499d5a30857df5ebb8bb4d8
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-27 20:40:46 -0600

    contacts class

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 
 
 class Contacts:
-    def __init__:
+    def __init__(self):
+        print('================')
+        print('  Contact List  ')
+        print('================')
+        print('please make a selection')
+        
