629e8bdbc13400c0146c811eda7ae54cd17b8b8f
Geoff Vincent <geoffkvincent@gmail.com>
2019-04-07 09:48:58 -0600

    view menu switch

--- main.py
+++ main.py
@@ -75,16 +75,19 @@ class ContactsList:
         cprint('=========================', 'magenta')
         cprint('Enter index to edit field', 'yellow')
         cprint('=========================', 'magenta')
-        contact_select = contact_select.items()
-        for index, (k,v) in enumerate(contact_select, 1):
-            print(f"{index}.{k}: {v}")
+        # contact_select = contact_select.items()
+        for index, (k, v) in enumerate(contact_select.items(), 1):
+            # import pdb; pdb.set_trace()
+            print(f"{index}. {k}  {v}")
         edit_option = int(input())
-        for index, (k,v) in enumerate(contact_select, 1):
+        for index, (k,v) in enumerate(contact_select.items(), 1):
             if index == edit_option:
                 edit_select = f"{k}: {v}"
                 print(edit_select)
                 contact_update = input('Enter updated info:')
-                contact_select['k'] = contact_update
+                contact_select[f"{k}"] = contact_update
+                print(contact_select[k])
+                print(contact_select)
 
     def delete_contact(self, contact_select):
         print(contact_select)
